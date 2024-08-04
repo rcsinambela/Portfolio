cde1f3c00a3486d423ba1d3b83661b895fd56a82
Rycs123 <riyanda.c.sinambela>
2024-08-04 07:07:24 +0700

    "Updated index.js, index.html, and package-lock.json files with changes to typed strings, personal information, and project images."

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,13 @@
 AOS.init();
 
 var typed = new Typed("#typedMe", {
-    strings: [
-        "an Informatics Student",
-        "a Full Stack Engineer",
-        "a Security Researcher",
-    ],
-    typeSpeed: 70,
-    loop: true,
-    loopCount: Infinity,
-    backSpeed: 70,
+  strings: [
+    "an Informatics Student",
+    "a Web Enthusiast",
+    "a Cyber Security Enthusiast",
+  ],
+  typeSpeed: 70,
+  loop: true,
+  loopCount: Infinity,
+  backSpeed: 70,
 });
